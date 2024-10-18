efe3d8364a1979ce786215ab2d2c6a2b416a28a6
gaoguangxiao <gaoguangxiao125@sina.com>
2024-10-18 10:30:19 +0800

    可对远程MP音频进行评测

--- QCloudSOE_iOS_v2.0.1/demo/demo/OralEvaluationViewController.m
+++ QCloudSOE_iOS_v2.0.1/demo/demo/OralEvaluationViewController.m
@@ -17,7 +17,7 @@
 #import "Slider.h"
 
 #import "demo-Swift.h"
-
+#import "GXTaskDownload-Swift.h"
 @interface OralEvaluationViewController () <TAIOralListener, UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *refText;
@@ -36,6 +36,9 @@
 @property (weak, nonatomic) IBOutlet UITextField *keywordText;
 
 @property (nonatomic, strong) SOE *recordSOE;
+
+//下载音频至沙盒
+@property (nonatomic, strong) GXDownloadManager *downloader;
 @end
 
 @implementation OralEvaluationViewController {
@@ -56,6 +59,40 @@
     [_sentenceInfoSeg setSelectedSegmentIndex:1];
     
     self.recordSOE = [SOE new];
+    self.downloader = [GXDownloadManager new];
+}
+
+- (void)initTAIConfig:(id<TAIOralDataSource>)source {
+    TAIOralConfig* config = [[TAIOralConfig alloc] init];
+    config.appID = kQDAppId;
+    config.token = [PrivateInfo shareInstance].token;
+    config.secretID = [PrivateInfo shareInstance].secretId;
+    config.secretKey = [PrivateInfo shareInstance].secretKey;
+    [config setApiParam:kTAIServerEngineType value:self.engineSeg.selectedSegmentIndex == 0 ? @"16k_en" : @"16k_zh"];
+    [config setApiParam:kTAIEvalMode value:[@(self.evalModeSeg.selectedSegmentIndex) stringValue]];
+    [config setApiParam:kTAIRefText value:self.refText.text];
+    [config setApiParam:kTAIScoreCoeff value:[@(self.coeffSlider.value) stringValue]];
+    [config setApiParam:kTAISentenceInfoEnabled value:[@(self.sentenceInfoSeg.selectedSegmentIndex) stringValue]];
+    if (self->_keywordText.text.length) {
+        [config setApiParam:kTAIKeyword value:self->_keywordText.text];
+    }
+    config.connectTimeout = 3000;
+    
+    if ([source isKindOfClass:RecordDataSource.class]) {
+        NSString *videoDestDateString = [self createFileNamePrefix];
+        //  config.audioFile =  [NSString stringWithFormat:@"%@/%@.pcm", NSTemporaryDirectory(),videoDestDateString];
+        config.audioFile = [NSString stringWithFormat:@"%@/%@.wav", NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0], videoDestDateString];
+        config.vadInterval = self->_vadSlider.value;
+        config.vadVolume = self->_vadVolumeSlider.value;
+    }
+    
+    self->_ctl = nil;
+    //    self->_source = nil;
+    
+    self->_ctl =  [config build:source listener:self];
+    self->_result = @"";
+    self->_running = true;
+    [self->_actionBtn setTitle:@"停止评测" forState:UIControlStateNormal];
 }
 
 - (IBAction)onClick:(id)sender {
@@ -66,47 +103,39 @@
             if(self->_running) {
                 [self->_ctl stop];
             }else{
-                TAIOralConfig* config = [[TAIOralConfig alloc] init];
-                config.appID = kQDAppId;
-                config.token = [PrivateInfo shareInstance].token;
-                config.secretID = [PrivateInfo shareInstance].secretId;
-                config.secretKey = [PrivateInfo shareInstance].secretKey;
-                [config setApiParam:kTAIServerEngineType value:self.engineSeg.selectedSegmentIndex == 0 ? @"16k_en" : @"16k_zh"];
-                [config setApiParam:kTAIEvalMode value:[@(self.evalModeSeg.selectedSegmentIndex) stringValue]];
-                [config setApiParam:kTAIRefText value:self.refText.text];
-                [config setApiParam:kTAIScoreCoeff value:[@(self.coeffSlider.value) stringValue]];
-                [config setApiParam:kTAISentenceInfoEnabled value:[@(self.sentenceInfoSeg.selectedSegmentIndex) stringValue]];
-                if (self->_keywordText.text.length) {
-                    [config setApiParam:kTAIKeyword value:self->_keywordText.text];
-                }
-                NSString *videoDestDateString = [self createFileNamePrefix];
-//                config.audioFile =  [NSString stringWithFormat:@"%@/%@.pcm", NSTemporaryDirectory(),videoDestDateString];
-                config.audioFile = [NSString stringWithFormat:@"%@/%@.wav", NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0], videoDestDateString];
-                config.vadInterval = self->_vadSlider.value;
-                config.vadVolume = self->_vadVolumeSlider.value;
-                config.connectTimeout = 3000;
-                self->_ctl = nil;
+                
                 self->_source = nil;
+                
                 if ([self->_sourceSeg selectedSegmentIndex] == 0) {
                     self->_source = [[RecordDataSource alloc] init];
+                    [self initTAIConfig:self-> _source];
                 } else {
                     // 文件源的pcm必须为单通道s16le格式
-                    NSString *path = [[NSBundle mainBundle] pathForResource:@"2024-10-16_10-00-27" ofType:@"wav"];
-//                    NSString *path = [NSString stringWithFormat:@"%@/%@", [[NSBundle mainBundle]bundlePath], @"how_are_you.wav"];
-                    self->_source = [[FileDataSource alloc] init:path];
-                    // 如果文件源不为pcm格式,可使用下面的方式
-        //             [config setApiParam:kTAIVoiceFormat value:@"2"];
-//
-//                    NSString*wavPath = [NSString stringWithFormat:@"%@/%@", [[NSBundle mainBundle]bundlePath], @"how_are_you.mp3"];
-//                    self->_source = [[AudioToolDataSource alloc] init:wavPath];
+                    //                    NSString *path = [[NSBundle mainBundle] pathForResource:@"2024-10-16_10-00-27" ofType:@"wav"];
+                    //                    NSString *path = [NSString stringWithFormat:@"%@/%@", [[NSBundle mainBundle]bundlePath], @"how_are_you.wav"];
+                    //                    self->_source = [[FileDataSource alloc] init:path];
+                    // 如果文件源不为pcm格式,可使用下面的方式//
+                    //                    NSString*wavPath = [NSString stringWithFormat:@"%@/%@", [[NSBundle mainBundle]bundlePath], @"how_are_you.mp3"];
+                    
+                    // 文件源为网络音频 https://file.risekid.cn/record/problem/68055/493/2/8c3c3533618547abb24176e73e3cc8f5.mp3
+                    NSString *mp3URL = @"https://file.risekid.cn/record/problem/68055/493/2/8c3c3533618547abb24176e73e3cc8f5.mp3";
+                    //                    data.audio = [NSData dataWithContentsOfFile:wavPath];
+                    //下载音频
+                    [self.downloader downloadV2WithUrl:mp3URL path:@"problem" priority:0 block:^(float progress, NSString * _Nullable audioPath) {
+                        //                        NSLog(@"audio path is: %@",audioPath);
+                        
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            self->_source = [[AudioToolDataSource alloc] init:audioPath];
+                            [self initTAIConfig:self-> _source];
+                        });
+                        
+                        
+                    }];
                 }
-                self->_ctl =  [config build:self->_source listener:self];
-                self->_result = @"";
-                self->_running = true;
-                [self->_actionBtn setTitle:@"停止评测" forState:UIControlStateNormal];
+                
             }
         });
-       
+        
         
     }];
 }
@@ -124,12 +153,12 @@
     return YES;
 }
 
-
-- (void)onError:(nonnull NSError *)error { 
+- (void)onError:(nonnull NSError *)error {
     [_source stop];
     _running = false;
     _result = [NSString stringWithFormat:@"%@\n%@", _result, error];
     [_resultText setText:_result];
+    NSLog(@"SOE onError ----> %@", _result);
     [_actionBtn setTitle:@"开始评测" forState:UIControlStateNormal];
 }
 
@@ -141,6 +170,7 @@
 
 //评测中收到的服务端信息
 - (void)onMessage:(nonnull NSString *)value {
+    NSLog(@"SOE onMessage ----> %@", value);
     _result = [NSString stringWithFormat:@"%@\n%@", _result, value];
     [_resultText setText:_result];
 }
